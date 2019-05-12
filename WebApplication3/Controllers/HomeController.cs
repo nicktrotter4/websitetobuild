1ae6dec0960e6e9c8923c5811f5797e86790709e
Nick Trotter <nicktrotter4@yahoo.com>
2019-05-12 14:15:10 -0400

    Add project files.

---
+++ WebApplication3/Controllers/HomeController.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication3.Models;
+using System.IO;
+using CsvHelper;
+using System.Diagnostics;
+
+namespace WebApplication3.Controllers
+{
+    public class HomeController : Controller
+    {
+        //Client Website Demo View
+        public ActionResult TestWebsite()
+        {
+
+            return View();
+        }
+
+
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(HttpPostedFileBase file, Clients clients)
+        {
+            string path = null;
+            List<Clients> Clientstodisplay = new List<Clients>();
+
+            if (file.ContentLength > 0)
+            {
+                var fileName = Path.GetFileName(file.FileName);
+                path = AppDomain.CurrentDomain.BaseDirectory + "upload\\" + fileName;
+                file.SaveAs(path);
+
+                var csv = new CsvReader(new StreamReader(path));
+                var ClientList = csv.GetRecords<Class1>();
+
+                foreach (var c in ClientList)
+                {
+                    Clients ClientToDisplay = new Clients();
+
+                    ClientToDisplay.Name = c.Name;
+                    ClientToDisplay.Phone = c.Phone;
+                    ClientToDisplay.City = c.City;
+                    ClientToDisplay.Zip = c.Zip;
+                    ClientToDisplay.Category = c.Category;
+                    ClientToDisplay.Email = c.Email;
+                    ClientToDisplay.Address = c.Address;
+
+                    Clientstodisplay.Add(ClientToDisplay);  
+                }
+
+      
+
+                
+            }
+
+            return View(clients);
+        }
+    }
+}
---
+++ WebApplication3/Models/Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication3.Models
+{
+    public class Class1
+    {
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public string Category { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string Zip { get; set; }
+
+    }
+
+    public class Clients
+    {
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public string Category { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string Zip { get; set; }
+
+      
+        
+    }
+}
